6922565b1d585600d9e8330fb4d1ed49863955d4
srinumeesala <srinusri76585@gmail.com>
2023-02-27 17:30:40 +0530

    nothing

--- app.js
+++ app.js
@@ -19,17 +19,26 @@ app.get("/weather", async (request, response) => {
     console.log("Accessed- Weather API")
 
     const { city, type } = request.query
-    console.log(city)
+    console.log(request.url)
     try {
-        weather.find({ search: city, degreeType: type }, function (err, result) {
-            if (err) console.log(err);
-            const res = JSON.stringify(result, null, 2)
-            console.log(res)
-            response.status(200)
-            response.send(JSON.parse(res))
+        weather.find({ search: city, degreeType: type }, (err, result) => {
+
+            if (err) {
+                console.log(err)
+                response.status(500)
+                response.send({ msg: "Something went wrong" })
+            } else {
+                // console.log(result)
+                // const res = JSON.stringify(result, null, 2)
+                // console.log(res)
+                response.status(200)
+                response.send(result)
+            }
         });
     } catch (err) {
         console.log(err)
+        response.status(500)
+        response.send({ msg: "Something went wrong" })
     }
 })
 
